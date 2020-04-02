0345f98b84fe0e1dc3c45ac144d1097d503ff9ca
Chris Gerber <chris.gerber@well.co>
2020-04-02 12:39:42 -0500

    Updates to bootstrap functions

--- lib/files.zsh
+++ lib/files.zsh
@@ -1,7 +1,8 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 
 
 function -dot-cache-source-file() {
+  # Source a file from the cache.
   local fh="${ZSH_CACHE_DIR}/$1"
 
   test -e "${fh}" && source "${fh}"
@@ -9,16 +10,18 @@ function -dot-cache-source-file() {
 
 
 function -dot-cache-update-file() {
+  # Update a file in the cache directory by overwriting the contents with the
+  # output of the passed command.
   local fh="${ZSH_CACHE_DIR}/$1" cmmd="$2"
-
   echo "Updating cached file ${fh}"
-
-  rm -f "${fh}" && (eval "${cmmd}") > "${fh}"
+  (eval "${cmmd}")&> "${fh}"
 }
 
 
 function -dot-source-dotfile() {
   # Source a file in the dotfile dir.
+  # If it doesn't find the matching file in the dotfile directory, it will
+  # source it from the current working directory.
   if [ -e "${__DD}/$1" ]; then
     source "${__DD}/$1";
   elif [ -e "${1}" ]; then
@@ -69,14 +72,23 @@ function -dot-autoload-completions() {
 
 
 function -dot-add-symlink-to-home() {
-  # Generates symlink in the $HOME directory
-  local src=$1 des=$2
+  # Creates symlink in the $HOME directory
+  local _src="$__DD/$1" _dest="$HOME/$2"
 
-  if [ -L $HOME/$des ] && [ -e $HOME/$des ]; then return 0; fi
-
-  test -d "$(dirname $HOME/$des)" || mkdir -p "$(dirname $HOME/$des)"
-
-  if [ ! -L $HOME/$des ]; then
-    ln -sf "$__DD/$src" "$HOME/$des"
+  if ! test -L $_dest; then
+    if ! test -e $_dest; then
+      if test -e "$_src"; then
+        ln -sf "$_src" "$_dest"
+        return 0
+      fi
+      printf \
+        "Unable to create symlink for %s; src file %s does not exist.\n" \
+        $_dest \
+        $_src
+    fi
+    printf \
+      "Unable to create symlink for %s; dest file %s already exists.\n" \
+      $_dest \
+      $_dest
   fi
 }
--- lib/maintenance.zsh
+++ lib/maintenance.zsh
@@ -1,56 +1,132 @@
 #!/bin/zsh
 
 
-function -dot-install-omz() {
-  # Install OMZ
-  test -d $ZSH && return
+function -dot-install-github-repo() {
+  # Idempotently clone repo from GitHub into directory.
+  # Usage:
+  # $1 (required) = Namespace/ProjectName
+  # $2 (required) = Filesystem Location
+  # $3            = Protocol (SSH|HTTPS)
 
-  git clone --depth=1 $OMZ_REPO $ZSH || {
-    printf "Error: git clone of oh-my-zsh repo failed\n"
-    echo 1
-    return 1
-  }
+  local __url __dir=$2 __protocol="${GIT_PROTOCOL:=ssh}"
+
+  if ! test -d $__dir; then mkdir -p $__dir; fi
+  local __test=$(git -C $__dir remote -v &>/dev/null)
+
+  if test $? -ne 0; then
+    if test "$3"; then __protocol="$3"; fi;
+
+    case $__protocol in
+      https|HTTPS) # Use HTTPS
+        __url="https://github.com/$1.git"
+        ;;
+      ssh|SSH|*)   # Default
+        __url="git@github.com:$1.git"
+        ;;
+    esac;
+
+    git clone --depth 10 $__url $__dir;
+  fi
 }
 
 
-function -upgrade-shell-env() {
-  # Upgrade the Shell Environment
+function -dot-install-omz() {
+  # Installs OMZ into the ZSH directory
+  -dot-install-github-repo \
+    "robbyrussell/oh-my-zsh" \
+    "${ZSH}:=${ZSH_CACHE_DIR}/oh-my-zsh"
+}
+
+
+function -dot-install-github-plugin() {
+  # Install Github Plugin
+
+  local name=$1 plugin_name=${1#*/} __dir="${2:=${ZSH_CUSTOM}/plugins}"
+
+  -dot-install-github-repo \
+    "$name" \
+    "${__dir}/${plugin_name}" \
+    "HTTPS";
+
+  plugins=($plugins $plugin_name)
+}
+
+
+function -dot-upgrade-shell-env() {
+  # Upgrade the Dotfiles environment
   -dot-upgrade-dotfiles-dir
+
   # We have to run the brew upgrade first since everything is installed by it.
   -dot-upgrade-brew
+
   # The Rest
-  -dot-upgrade-omz
+  -dot-upgrade-cache-repos
   -dot-upgrade-zsh-plugins
   -dot-upgrade-dotfiles-projects
 
+  # Lastly, reload the shell
   eval "${SHELL}"
+}
+
+
+alias -upgrade-shell-env="-dot-upgrade-shell-env"
+
+
+function -dot-upgrade-cache-repos() {
+  # Update cache directory repositories
+  local __cachedir="${ZSH_CACHE_DIR:=$__DD/.cache}"
+  local __basedir="$(dirname $__cachedir)"
+
+  for i in $(ls ${__cachedir}); do
+    (
+      set -v
+      git -C ${__basedir}/$i pull origin master
+    ) &
+  done
+
+  wait
+}
+
+
+function -dot-upgrade-zsh-plugins() {
+  # Update plugins from Github
+  local _plugins_dir="${ZSH_CUSTOM}/plugins"
+
+  for i in $(ls ${_plugins_dir}/); do
+    (
+      set -v
+      git -C ${_plugins_dir}/$i pull origin master
+    ) &
+  done
+
+  wait
 }
 
 
 function -dot-upgrade-dotfiles-projects() {
   # Run upgrade.zsh across project
   for i in $(ls -d $__DD/*/upgrade.zsh); do
-    (set -v; source "${i}") &
+    (
+      set -v
+      source "${i}"
+    ) &
   done
+
   wait
 }
 
 
 function -dot-upgrade-dotfiles-dir() {
+  # Update the dotfiles directory, caching the contents while updating.
   (
     set -v
     git -C $__DD stash
     git -C $__DD pull --ff-only origin master || true
-    git -C $__DD stash pop
-  ) &
-  wait
-  -dot-source-dotfile "init.zsh"
-}
+  )
 
+  (git -C $__DD stash pop || true) &>/dev/null
 
-function -dot-upgrade-omz() {
-  # Update ZSH
-  (set -v; git -C "${ZSH}" pull origin master)
+  -dot-main
 }
 
 
@@ -58,7 +134,7 @@ function -dot-upgrade-brew() {
   # Upgrade Homebrew
   local _update_args _upgrade_args _dump_args _cmd
 
-  { # Update
+  { # Update brew
     _update_args="--force"
     if [[ -n "$ZSH_DEBUG" ]]; then _update_args="--verbose ${_update_args}"; fi
     _cmd="brew update $_update_args"
@@ -66,7 +142,7 @@ function -dot-upgrade-brew() {
     eval "${_cmd}"
   }
 
-  { # Upgrade
+  { # Upgrade brews
     _upgrade_args="--display-times"
     if [[ -n "$ZSH_DEBUG" ]]; then _upgrade_args="--verbose ${_upgrade_args}"; fi
     _cmd="brew upgrade $_upgrade_args"
@@ -74,14 +150,14 @@ function -dot-upgrade-brew() {
     eval "${_cmd}"
   }
 
-  ( # Dump
+  ( # Dump installed brews to file.
     if [ -n "$BREW_FILE" ]; then
       _cmd="brew bundle dump --force --all --describe --file=${BREW_FILE}"
       eval "${_cmd}"
     fi
   ) &
 
-  ( # Cleanup
+  ( # Cleanup cached brews
     _cmd="brew cleanup --verbose --prune=${BREW_CLEANUP_PRUNE_DAYS}"
     eval "${_cmd}"
   ) &
